6762bb4b1e456b5e900258c14933255e3de01f01
Subi Hwang <ghkdtnql@gmail.com>
2023-08-16 21:09:37 +0900

    Initial Commit

---
+++ Hey_Groot/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hey_Groot
+//
+//  Created by 황수비 on 2023/08/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
