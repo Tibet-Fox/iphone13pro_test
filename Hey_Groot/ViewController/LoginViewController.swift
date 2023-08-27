cda0348c6c328e036f2099d64d436aa7a171b57e
Subi Hwang <ghkdtnql@gmail.com>
2023-08-27 13:58:00 +0900

    commit

---
+++ Hey_Groot/ViewController/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  Hey_Groot
+//
+//  Created by 황수비 on 2023/08/20.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var loginBtn: UIButton!
+    override func viewDidLoad() {
+        
+    }
+    
+    
+    @IBAction func pressLoginBtn(_ sender: UIButton) {
+        
+        
+        
+    }
+}
