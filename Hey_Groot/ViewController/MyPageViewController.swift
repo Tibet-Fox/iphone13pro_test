3ec43136189c4b90a350898a9e916b461f21126e
Subi Hwang <ghkdtnql@gmail.com>
2023-09-04 13:07:46 +0900

    commit

--- Hey_Groot/ViewController/MyPageViewController.swift
+++ Hey_Groot/ViewController/MyPageViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class MyPageViewController: UIViewController{
     
     override func viewDidLoad() {
-        
+        view.backgroundColor = UIColor(red: 0.988, green: 0.98, blue: 0.945, alpha: 1)
     }
     
 }
--- Hey_Groot/ViewController/TabBarViewController.swift
+++ Hey_Groot/ViewController/TabBarViewController.swift
@@ -13,30 +13,33 @@ class TabBarViewController: UITabBarController, UITabBarControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.delegate = self
+//        self.tabBar.backgroundImage = UIColor(red: 123/255, green: 193/255, blue: 178/255, alpha: 1)
+        self.tabBar.tintColor = UIColor(red: 0.6, green: 0.808, blue: 0.506, alpha: 1)
+        self.tabBar.unselectedItemTintColor = .black
         
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        
+
+
+
+
         let tabOne = HomeViewController()
-        let tabOneBarItem = UITabBarItem(title: "탭 1", image: UIImage(named: "tab1"), tag: 0)
+        let tabOneBarItem = UITabBarItem(title: "홈", image: UIImage(named: "home"), tag: 0)
         tabOne.tabBarItem = tabOneBarItem
-        
+
         let tabTwo = UINavigationController(rootViewController: SearchViewController())
-        let tabTwoBarItem = UITabBarItem(title: "탭 2", image: UIImage(named: "tab2"), tag: 1)
+        let tabTwoBarItem = UITabBarItem(title: "검색", image: UIImage(named: "search"), tag: 1)
         tabTwo.tabBarItem = tabTwoBarItem
-        
+
         let tabThree = UINavigationController(rootViewController: MyPageViewController())
-        let tabThreeBarItem = UITabBarItem(title: "탭 3", image: UIImage(named: "tab3"), tag: 2)
+        let tabThreeBarItem = UITabBarItem(title: "내 정보", image: UIImage(named: "user"), tag: 2)
         tabThree.tabBarItem = tabThreeBarItem
-     
+
         self.viewControllers = [tabOne, tabTwo, tabThree]
-        
-        
     }
-    
+
    
 
 }
