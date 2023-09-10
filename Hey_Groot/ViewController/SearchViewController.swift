eb2e97e18138f74f7b7f2dedd1c579e3eca7db2e
soomin0224 <soomin0224@icloud.com>
2023-09-10 16:02:52 +0900

    commit

--- Hey_Groot/ViewController/SearchViewController.swift
+++ Hey_Groot/ViewController/SearchViewController.swift
@@ -11,7 +11,6 @@ import UIKit
 class SearchViewController: UIViewController{
     
     override func viewDidLoad() {
-        view.backgroundColor = UIColor(red: 0.988, green: 0.98, blue: 0.945, alpha: 1)
         
     }
     
